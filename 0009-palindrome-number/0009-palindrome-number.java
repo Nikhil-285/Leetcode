97aa11cee84f9693bc0becbc39be54592281ecf7
Nikhil Patil <np49959@gmail.com>
2026-08-04 22:42:45 +0530

    Time: 5 ms (80.99%), Space: 46.1 MB (33.94%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        long temp=x;
+        long num=0;
+        while(temp>0){
+            num=temp%10 + (num*10);
+            temp=temp/10;
+        }
+        if(num==x){
+            return true;
+        }
+        return false;
+    }
+}
