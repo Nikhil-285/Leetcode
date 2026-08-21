85152319f3e9d0509c38050fca86714c14283b20
Nikhil Patil <np49959@gmail.com>
2026-08-21 21:40:21 +0530

    Fix running sum calculation in the method

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,9 +1,13 @@
 class Solution {
     public int[] runningSum(int[] nums) {
         int runsum[]=new int[nums.length];
+
+        
           runsum[0]=nums[0];
+
+        
         for(int i=1;i<nums.length;i++){
-           
+
            runsum[i]=nums[i]+runsum[i-1];
         }
         return runsum;
